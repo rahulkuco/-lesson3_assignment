ea21b66bd57cab68f8311b44ee53292a162448b2
rahulkotturi <rkotturi@uco.edu>
2021-09-30 21:09:15 -0500

    initial commit

---
+++ lesson3/controller/ShapeEventListener.java
@@ -0,0 +1,53 @@
+package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.Color;
+
+import model.Circle;
+import model.Rectangle;
+import model.Shape;
+import view.MenuScreen;
+import view.ShapePanel;
+public class ShapeEventListener implements ActionListener {
+
+    private ShapePanel panel;
+    public ShapeEventListener(ShapePanel panel){
+        this.panel=panel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        
+     JButton button = (JButton) e.getSource();
+        if (button == panel.getExitButton()){
+            JFrame window = panel.getWindow();
+            window.getContentPane().removeAll();
+            var menu= new MenuScreen(window);
+            menu.init();
+            window.pack();
+            window.revalidate();;
+        }
+        else if(button==panel.getRandomShapeButton()){
+          ArrayList<Shape>shapes = panel.getCanvas().getShapes();
+          shapes.clear();
+
+          Random random = new Random();
+          for(int a=0;a<200;a++)
+          {
+              int r = random.nextInt(155)+100;
+              int g = random.nextInt(155)+100;
+              int b = random.nextInt(155)+100;
+              Color c = new Color(r,g,b);
+             shapes.add(new Circle(random.nextInt(500),random.nextInt(500),c, 25));
+             shapes.add(new Rectangle(random.nextInt(500),random.nextInt(500),c, 25,25));
+          }
+          panel.getCanvas().repaint();
+        }
+    }
+    
+}
